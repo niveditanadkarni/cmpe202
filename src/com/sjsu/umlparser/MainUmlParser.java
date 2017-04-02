1b1618fc367fa459c254a33dabd1e1ad2975c70c
niveditanadkarni <nivedita.nadkarni@sjsu.edu>
2017-04-01 21:50:16 -0700

    new commit.

--- src/com/sjsu/umlparser/MainUmlParser.java
+++ src/com/sjsu/umlparser/MainUmlParser.java
@@ -3,7 +3,7 @@ package com.sjsu.umlparser;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.nio.file.Path;
 
 
 public class MainUmlParser {
@@ -12,6 +12,11 @@ public class MainUmlParser {
 		// TODO Auto-generated method stub
 		String file = "Users/niveditanadkarni/cmpe202-master/umlparser/uml-parser-test-1/A.java";
 		System.out.println(file);
+		
+		//if (!file.isDirectory())
+		//{
+		//	 file = file.getParentFile();
+		//}
 
 	}
 
