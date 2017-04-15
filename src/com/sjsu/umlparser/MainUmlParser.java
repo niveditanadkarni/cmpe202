9217e5bc4352a7ca6e090fb10ca97b8dcd9de1b1
niveditanadkarni <nivedita.nadkarni@sjsu.edu>
2017-04-15 00:11:58 -0700

    latest commit

--- src/com/sjsu/umlparser/MainUmlParser.java
+++ src/com/sjsu/umlparser/MainUmlParser.java
@@ -1,16 +1,21 @@
 package com.sjsu.umlparser;
 
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 
 public class MainUmlParser {
 
-	private static File files= null;
-	private static File[] listoffiles= null;
+	//private static File files= null;
+	//private static File[] listoffiles= null;
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		String file = "Users/niveditanadkarni/cmpe202-master/umlparser/uml-parser-test-1/A.java";
@@ -20,11 +25,33 @@ public class MainUmlParser {
 		if(f.exists() && !f.isDirectory()) { 
 		    // do something
 		}
+		File input_directory = new File(args[0]);
+		File[] input_javafiles = input_directory.listFiles();
+		if (input_javafiles != null) {
+
+			/* array of class names */
+
+			for (File classname : input_javafiles) {
+
+				ArrayList<String> tokenisation = new ArrayList<String>();
+				if ((classname.getName().contains(".java"))) {
+
+					File file = new File(classname.getPath());
+
+					FileInputStream in = new FileInputStream(file);
+					CompilationUnit cu;
+					try {
+
+						cu = JavaParser.parse(in, "UTF8");
+					}
+
+					finally {
+						in.close();
+					}
 		//if (!file.isDirectory())
 		//{
 		//	 file = file.getParentFile();
 		//}
-
+				
 	}
 
-}
