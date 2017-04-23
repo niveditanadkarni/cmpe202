6c78b3ac4b25ec5279b97c65760480f8dd26f00a
niveditanadkarni <nivedita.nadkarni@sjsu.edu>
2017-04-22 23:47:58 -0700

    latest Commit.

--- src/com/sjsu/umlparser/MainUmlParser.java
+++ src/com/sjsu/umlparser/MainUmlParser.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.rmi.ssl.SslRMIClientSocketFactory;
 
 public class MainUmlParser {
