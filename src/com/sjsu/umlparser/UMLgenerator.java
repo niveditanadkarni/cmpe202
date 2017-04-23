1f91055e529434e32ad74b127ac9576cc759303b
niveditanadkarni <nivedita.nadkarni@sjsu.edu>
2017-04-22 23:37:20 -0700

    UMLgenerator.

---
+++ src/com/sjsu/umlparser/UMLgenerator.java
@@ -0,0 +1,31 @@
+package com.sjsu.umlparser;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import net.sourceforge.plantuml.SourceStringReader;
+
+public class UMLgenerator {
+public  void umlCreator(String source,String destination) {
+		
+		OutputStream image = null;
+		try {
+			image = new FileOutputStream(destination);
+			System.out.println("Package");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			SourceStringReader reader = new SourceStringReader(source);
+		
+		try {
+			reader.generateImage(image);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Return a null string if no generation
+	}
+}
